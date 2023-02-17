85c46de66e1a83a214214eafbed789b176e86f65
lungileLuh <lungileprecious30@yahoo.com>
2023-02-17 01:51:04 -0800

    my first c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
-/*
- * main - print a line of code using puts
- * Return: 0
+#include <stdio.h>
+/**
+ * main -print a line of code using puts
+ *
+ * Return: 0 
  */
 int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle"):
-		return (0)
+{	puts("\"programming is like building a multilingual puzze"):
+	return (0):
+}		
