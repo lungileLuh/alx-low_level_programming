af848f43a1ecc3d516b692eb648166299a1cbac9
lungileLuh <lungileprecious30@yahoo.com>
2023-02-17 02:36:32 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,12 @@
 /**
  * main -print a line of code using puts
  *
- * Return: 0 
- */
+ * Return:0
+*/
+
 int main(void)
-{	puts("\"programming is like building a multilingual puzze"):
+{
+	puts("\"programming is like building a multilingual puzzle"):
 	return (0):
-}		
+}
+
