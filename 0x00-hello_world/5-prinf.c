a53645079caa463cd92fe96d1f8bca7b85e0fd5f
lungileLuh <lungileprecious30@yahoo.com>
2023-02-17 07:53:26 -0800

    5

--- 0x00-hello_world/5-prinf.c
+++ 0x00-hello_world/5-prinf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - print a C program using printf
+ * main - print a c  program using printf
  *
  * Return: 0
 */
 int main(void)
 {
-	printf("with proper grammar; but the outcome is a piece of art;\n")
-	return (0):
+	printf("with proper grammar,  but the outcome is a piece of art,\n");
+	return (0);
 }
