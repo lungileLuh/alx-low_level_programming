5c123c716c83d07d3d721c41bc5757328c552184
lungileLuh <lungileprecious30@yahoo.com>
2023-02-22 18:56:18 -0800

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter  =  'a'; letter  <=  '='  ; letter)
+	    			
+		_putchar(letter')';
+
+	_putchar('\n');
+'}
+
+
