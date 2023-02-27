1e72932c70890e9f2f8d0878b1b1f4d0ce56a797
lungileLuh <lungileprecious30@yahoo.com>
2023-02-27 15:20:06 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,15 +13,13 @@ void print_rev(char *s)
 	{
 		longi++;
 		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
 	{
-       s--; 
-  for (o = longi; o > 0; o--)
-  {
-	      _putchar(*s);
-	       s--;
-  }
+		_putchar(*s);
+		 s--;
+	}
 
-	 _putchar('\n');
+	_putchar('\n');
 }
-
-
