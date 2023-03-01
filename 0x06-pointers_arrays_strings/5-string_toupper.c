bdf5208d2adbf99f5deb4af1558eab2e336aeb13
lungileLuh <lungileprecious30@yahoo.com>
2023-03-01 11:13:25 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,14 +10,17 @@
 char *string_toupper(char *n)
 
 {
-	int j;
+	int i;
 
 	i = 0;
 	while (n[i] != '\0')
+
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
 			n[i] = n[i] - 32;
-		i++;
+		i++
+
 	}
 	return (n);
+
 }
