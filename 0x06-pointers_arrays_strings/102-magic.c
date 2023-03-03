16573d7d91bb1381edfff5c999c336c88f21fc33
lungileLuh <lungileprecious30@yahoo.com>
2023-03-02 23:43:11 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ p = &n;
  * - you are not allowed to use a
  * - you are not allowed to modify p
  * - only one statement
- * you are not allowed to code anything else than this line of code
+ * -you are not allowed to code anything else than this line of code
  */
 
 *(p + 5) = 98;
