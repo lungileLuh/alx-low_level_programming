6d28b3afa9d021525acf8fe3cfeb90a26291045f
lungileLuh <lungileprecious30@yahoo.com>
2023-03-03 02:02:27 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,27 +1,29 @@
-#include "main"
-#include <stdio.h>
+#include "main.h"
+
+
 /**
  * more_numbers - print more numbers
  */
+
 void more_numbers(void)
+
 {
 	int i, j;
 
 	for (i = 1; i <= 10; i++)
+
 	{
 		for (j = 0; j <= 14; j++)
+
 		{
 			if (j >= 10)
-			{
-				putchar('1');
-			}
-			putchar(j % 10 + '0');
+				_putchar('1');
+			_putchar (j % 10 + '0');
+
 		}
-		putchar('\n');
+
+		_putchar('\n');
+
 	}
+
 }
-
-
-
-
-
