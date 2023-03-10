8a7d6cc87bb83fdb762cace12b9b2a7ae8db155d
lungileLuh <lungileprecious30@yahoo.com>
2023-03-10 09:15:47 -0800

    update

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * _puts - program to print message using puts following by a new line
+ * @s: string to be considered
+ * Return: 0 (Success)
+ */
+void _puts(char *s)
+{
+	puts(s);
+
+}
