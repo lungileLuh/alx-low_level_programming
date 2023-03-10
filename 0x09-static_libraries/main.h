32f93b39f405e63553467582144f14fe3588acc9
lungileLuh <lungileprecious30@yahoo.com>
2023-03-10 09:47:40 -0800

    update

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+ #ifndef MAIN_H
+
+ #define MAIN_H
                 
 int _putchar(char c);                
 
@@ -41,4 +42,4 @@ char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
 
-#endif
+ #endif
