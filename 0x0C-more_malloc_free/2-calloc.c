9be7c3bbe2e21f6628572fed9f9b103689adedc3
lungileLuh <lungileprecious30@yahoo.com>
2023-03-16 04:16:14 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -26,6 +26,7 @@ char *_memset(char *s, char b, unsigned int n)
  * @nmemb: number of elements in the array
  * @size: size of each element
  *
+ * Return: pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
