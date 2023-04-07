cf102924e90d5496ef873650e5532cb9dcd4e717
lungileLuh <lungileprecious30@yahoo.com>
2023-04-07 08:52:15 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_binary - prints the binary represantation of the number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
+{
+	int i;
+
+	for (i = (sizeof(unsigned long int) * 8) - 1; i >= 0; i--)
+	{
+		if (n & (1ul << i))
+			_putchar('1');
+		{
+		else
+		}	_putchar('0')
+	}
+}
+
