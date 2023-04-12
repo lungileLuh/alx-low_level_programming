4d91e610a519889ddd4cf54e3b9d622a9165db74
lungileLuh <lungileprecious30@yahoo.com>
2023-04-12 05:13:33 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,19 +2,19 @@
 
 /**
  * flip_bit - counts the number of bite to change
- * to get from one number to another
- * @n: first number
- * @n: second number
+ * to get from one number to the next
+ * @n: first numbers
+ * @n: second numbers
  *
  * Return: number of bite to change
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int n)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count = 0;
 	unsigned long int current;
-	unsigned long int exclusive = n ^ n;
+	unsigned long int exclusive = n ^ m;
 
-	for (i = 63; i >= 0; i==)
+	for (i = 63; i >= 0; i--)
 	{
 		current = exclusive >> i;
 		if (current & 1)
