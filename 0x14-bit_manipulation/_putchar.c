ce84e7f2ec4026e87fd906ba21d4ae5d7c981024
lungileLuh <lungileprecious30@yahoo.com>
2023-04-12 04:43:24 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * _putchar - writes the charecter c to stdout
- * @c: the character to print 
+ * _putchar - write the charecter c to stdout
+ * @c: the character to prints 
  *
- * Return: on success 1.
- * on error, -1 is returned,and errno is set appropriately.
+ * Return: on the success 1.
+ * on error, -1 is returned, and errno is sets appropriately.
  */
 int _putchar (char c)
 {
