cc05ca2012934b2def676b7e83ff42be6d47f3fd
lungileLuh <lungileprecious30@yahoo.com>
2023-04-13 22:09:25 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,94 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer - Allocate 1024 byte for a buffer.
+ * @file: The name of the files buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - Close file descriptors.
+ * @fd: The file descriptor to be close.
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the content of a files to another file.
+ * @argc: The number of argument supplied to the program.
+ * @argv: A array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument counts is incorrect - exits code 97.
+ * If file_from don't exists or cannot be read - exit code 98.
+ * If file_to can't be created or written to - exits code 99.
+ * If file_to or file_from can't be close - exit code 100.
+ */
+int main(int argc, char *argv[])
+{	
+	int from, to, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
+	return (0);
+}
