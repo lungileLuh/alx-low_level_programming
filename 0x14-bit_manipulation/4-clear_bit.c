8348811feaeabf526ceef2e3ddc317feffd462a5
lungileLuh <lungileluthuli27@gmail.com>
2023-07-24 07:08:58 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+
+#include "main.h"
 
 /**
  * clear_bit - Sets the value of a bit to 0 at a given index.
@@ -10,36 +10,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
-	{
-		/* Error: Index is out of range. */
+	if (index > 63)
 		return (-1);
-	}
 
-	unsigned long int mask = 1UL << index;
-	*n &= ~mask;
-
+	*n = (~(1UL << index) & *n);
 	return (1);
-}
-
-#include <stdio.h>
-#include "main.h"
-
-int main(void)
-{
-	unsigned long int num = 42; /* Change this number to test different inputs */
-	unsigned int index = 3;    /* Change this index to test different positions */
-
-	int result = clear_bit(&num, index);
-
-	if (result == 1)
-	{
-		printf("Bit at index %u in %lu is now set to 0: %lu\n", index, num, num);
-	}
-	else
-	{
-		printf("Error: Index out of range!\n");
-	}
-
-	return (0);
 }
