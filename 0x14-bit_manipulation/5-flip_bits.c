b9c563e0ec66447d6d3f9661d6e94344f9928bd9
lungileLuh <lungileluthuli27@gmail.com>
2023-07-24 08:07:44 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,23 +13,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor_result = n ^ m;
+	unsigned long int exclusive = n ^ m;
 	unsigned int count = 0;
 
-	while (xor_result != 0)
+	while (exclusive != 0)
 	{
-		count += xor_result & 1;
-		xor_result >>= 1;
+		count += exclusive & 1;
+		exclusive >>= 1;
 	}
-	return (count);
-}
 
-int main(void)
-{
-	unsigned long int num1 = 42; /* Change this number to test different inputs */
-	unsigned long int num2 = 17; /* Change this number to test different inputs */
-
-	unsigned int result = flip_bits(num1, num2);
-	printf("Number of bits to flip from %lu to %lu: %u\n", num1, num2, result);
-	return (0);
+	return count;
 }
